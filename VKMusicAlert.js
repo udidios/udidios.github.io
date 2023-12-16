3d9489efe5256e034da09b026d519406bdb19d12
udidios <99913860+udidios@users.noreply.github.com>
2023-12-16 12:39:26 +0300

    Update VKMusicAlert.js

--- VKMusicAlert.js
+++ VKMusicAlert.js
@@ -1,1 +1,10 @@
-
+let body = JSON.parse($response.body)
+body.data.user.billing = {
+        "expiryDate": 4092657620,
+        "subscriptionRegion": "",
+        "state": "active",
+        "rank": 2,
+        "trialAvailable": true,
+        "comboAvailable": false
+      }
+$done({body: JSON.stringify(body)})
